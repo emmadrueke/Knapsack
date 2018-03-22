f731283df5f669ba26c94033aaeeb41cd5bbf85e
Emma Drueke <emma.drueke@gmaila.com>
2018-03-22 16:35:07 -0500

    First Commit

--- knapsack-generator.js
+++ knapsack-generator.js
@@ -8,8 +8,8 @@ if (args.length != 2) {
   process.exit(2);
 }
 
-let [filename, size] = args;
-const filePath = path.resolve(__dirname, filename);
+let [filename, size] = args; //??? so what does this mean???
+const filePath = path.resolve(__dirname, filename);// creates a new path
 
 if (fs.existsSync(filePath)) {
   console.error(`File "${filePath}" already exists!`);
@@ -21,18 +21,19 @@ if (isNaN(size)) {
   process.exit(2);
 }
 
-size = ~~Number(size);
-
+size = ~~Number(size); //~~ means Math.floor()
 let text = '';
 
-for (let i = 1; i <= size; i++)
+for (let i = 1; i <= size; i++) //What is happening??
   text += `${i} ${Math.floor(Math.random() * 101)} ${Math.floor(Math.random() * 101)}${i === size ? '' : '\n'}`;
-
+//I'm Crying//
 fs.writeFile(filePath, text, (error) => {
     if(error) {
       console.error('The file could not be saved');
       process.exit(1);
-    }
+    } 
 
     console.log(`Knapsack problem saved to ${filePath}`);
 });
+
+
